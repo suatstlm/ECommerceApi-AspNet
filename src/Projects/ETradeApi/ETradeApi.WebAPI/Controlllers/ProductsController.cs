2a87f33c37412518f9cbeb6e9fa76b61b985526b
SUAT SATILMIŞ <suat.satilmis@ym.com.tr>
2022-11-11 13:41:26 +0300

    Created ProductsControl

---
+++ src/Projects/ETradeApi/ETradeApi.WebAPI/Controlllers/ProductsController.cs
@@ -0,0 +1,18 @@
+﻿using ETradeApi.Application.Features.Products.Commands.CreateBrand;
+using ETradeApi.Application.Features.Products.Dtos;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ETradeApi.WebAPI.Controlllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProductsController : BaseController
+    {
+        [HttpPost]
+        public async Task<IActionResult> Add([FromBody] CreateProductCommand createProductEntityCommand)
+        {
+            CreatedProductDto result = await Mediator.Send(createProductEntityCommand);
+            return Created("", result);
+        }
+    }
+}
